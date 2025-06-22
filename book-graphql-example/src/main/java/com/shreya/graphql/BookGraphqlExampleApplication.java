947c87d30f4caf112f2bb39548c437e35aafc32a
shreya mahalle <shreyamahalle2000@gmail.com>
2025-06-22 15:50:05 +0530

    changes added

---
+++ book-graphql-example/src/main/java/com/shreya/graphql/BookGraphqlExampleApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookGraphqlExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookGraphqlExampleApplication.class, args);
+	}
+
+}
---
+++ book-graphql-example/src/test/java/com/shreya/graphql/BookGraphqlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreya.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookGraphqlExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
